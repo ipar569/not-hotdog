4f538a8972c8bdf4d1c0b62a8481698b55bea187
Injae Park <ipar569@auckland.uni.ac.nz>
2017-06-10 15:02:22 +1200

    Added taking photo feature to the app

--- Tabs/Tabs/Tabs/CustomVision.xaml.cs
+++ Tabs/Tabs/Tabs/CustomVision.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 
 namespace Tabs
 {
@@ -15,6 +12,34 @@ namespace Tabs
         public CustomVision()
         {
             InitializeComponent();
+        }
+
+        private async void loadCamera(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium,
+                Directory = "Sample",
+                Name = $"{DateTime.UtcNow}.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                return file.GetStream();
+            });
+
+            file.Dispose();
         }
     }
 }
